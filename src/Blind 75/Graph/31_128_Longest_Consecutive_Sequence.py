9d7057e8079f60cd553593232146f9f004dc21e1
Joseph Ciaravella <joseph.ciaravella@mail.mcgill.ca>
2025-01-21 20:30:19 -0500

    solved with hashset instead of dfs

--- src/Blind 75/Graph/31_128_Longest_Consecutive_Sequence.py
+++ src/Blind 75/Graph/31_128_Longest_Consecutive_Sequence.py
@@ -21,9 +21,28 @@ def longestConsecutive(nums):
     
     return longest_path
 
-print(longestConsecutive([100,4,200,1,3,2])) #4
-print(longestConsecutive([0,3,7,2,5,8,4,6,0,1])) # 9
-print(longestConsecutive([1,2,0,1])) # 3
-print(longestConsecutive([0])) # 1
-print(longestConsecutive([0,0])) # 1
-print(longestConsecutive([])) # 0
+# print(longestConsecutive([100,4,200,1,3,2])) #4
+# print(longestConsecutive([0,3,7,2,5,8,4,6,0,1])) # 9
+# print(longestConsecutive([1,2,0,1])) # 3
+# print(longestConsecutive([0])) # 1
+# print(longestConsecutive([0,0])) # 1
+# print(longestConsecutive([])) # 0
+
+
+def longest2(nums):
+    numss = set(nums)
+    longest = 0
+    for num in numss:
+        sub = 1
+        if num-1 not in numss:
+            while num+1 in numss:
+                sub += 1
+                num += 1
+
+        longest = sub if sub > longest else longest
+    
+    return longest
+
+print(longest2([1,2,0,1])) #3
+print(longest2([2,20,4,10,3,4,5])) #4
+print(longest2([9,1,4,7,3,-1,0,5,8,-1,6])) #7
