930298bb5c241d72a703fc60ed05f405a89af29b
Joseph Ciaravella <joseph.ciaravella@mail.mcgill.ca>
2025-01-31 10:49:04 -0500

    Daily leetcode

---
+++ src/Blind 75/Linked List/Reverse_Linked_List_206.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverseList(head):
+    prev = None
+    curr = head
+    while curr:
+        tmp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = tmp
+    return prev
+        
+        
