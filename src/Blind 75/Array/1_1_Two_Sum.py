77295a62b6d90626f4321f6c34cdc0ae680fb617
Joseph Ciaravella <joseph.ciaravella@mail.mcgill.ca>
2025-10-29 17:09:25 -0400

    optimal

--- src/Blind 75/Array/1_1_Two_Sum.py
+++ src/Blind 75/Array/1_1_Two_Sum.py
@@ -12,3 +12,20 @@ def twoSum(nums, target):
         rem = target - nums[i]
         if rem in hashtable and hashtable[rem] != i :
             return [i, hashtable[rem]]
+        
+
+
+def twoSum2(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    diff = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement not in diff:
+            diff[num] = i
+        else :
+            return [i, diff[complement]]
+# twoSum([3,2,4])
